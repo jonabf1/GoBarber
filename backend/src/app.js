c9ed0263ce4901b7da542335ee879ff96903f70a
Lacrimosa <jonabf1@gmail.com>
2019-12-26 03:24:39 -0300

    chore: cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 
 import express from 'express';
-import cors from 'cors';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
