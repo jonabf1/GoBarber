e374c4804160fa067ac3c99b0f69c98786b7c300
Lacrimosa <jonabf1@gmail.com>
2019-12-27 12:14:38 -0300

    fix: error in url image header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -26,8 +26,9 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
-                "https://api.adorable.io/avatars/50/abott@adorable.png"
+                profile.avatar
+                  ? profile.avatar.url
+                  : "https://api.adorable.io/avatars/50/abott@adorable.png"
               }
               alt="avatar"
             />
