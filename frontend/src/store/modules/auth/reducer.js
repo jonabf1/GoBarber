8399f8c57ed3367b99b7496031884ce93347853e
Lacrimosa <jonabf1@gmail.com>
2019-12-27 12:59:04 -0300

    chore: Disable loading in SignOut

--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -26,6 +26,7 @@ export default function auth(state = INITIAL_STATE, action) {
       case "@auth/SIGN_OUT": {
         draft.token = null;
         draft.signed = false;
+        draft.loading = false;
         break;
       }
       default:
